84f51c1ade3f340a1d7bdbe6685fdede82a522b1
agungdunggio <agngdnggio@gmail.com>
2024-05-08 14:17:59 +0700

    add map component

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,24 @@
-import { useState } from 'react'
-import { Routes, Route } from 'react-router-dom'
-import WACS from './components/WA-CS'
-import Home from './pages/Home'
-import Map from './pages/Map'
-import Tabular from './pages/Tabular'
-import './App.css'
-
+import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import WACS from "./components/WA-CS";
+import Home from "./pages/Home";
+import Map from "./pages/Map";
+import Tabular from "./pages/Tabular";
+import "./App.css";
 
 function App() {
-
   return (
     <>
       <div className="routing">
         <Routes>
-          <Route path='/' element={<Home/>} exact/>
-          <Route path='/peta' element={<Map/>}/>
-          <Route path='/data' element={<Tabular/>}/>
+          <Route path="/" element={<Home />} exact />
+          <Route path="/peta" element={<Map />} />
+          <Route path="/data" element={<Tabular />} />
         </Routes>
       </div>
       <WACS />
-
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/pages/Map.jsx
+++ src/pages/Map.jsx
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useRef, useState } from "react";
+
+import { Map, TileLayer } from "react-leaflet";
 
 const Map = () => {
-    return (
-        <>
-            Map page
-        </>
-    )
-}
+  const [center, setCenter] = useState({ lat: 13.0844622, lng: 80.248357 });
+  const ZOOM_LEVEL = 9;
+  const mapRef = useRef();
+
+  return (
+    <>
+      <Map center={center} zoom={ZOOM_LEVEL} ref={mapRef}>
+        <TileLayer
+          url={"https://api.maptiler.com/maps/basic-v2/256/{z}/{x}/{y}.png?key=UwNIQPKbRyGGA9A86zvC"}
+          attribution={'<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors'}
+        />
+      </Map>
+    </>
+  );
+};
 
 export default Map;
