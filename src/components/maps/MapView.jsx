0721ae1aba8a0275cad3ce417f509db6c93111d4
agungdunggio <agngdnggio@gmail.com>
2024-05-13 03:42:51 +0700

    feat(animation): add checkbox jaringan irigasi

--- src/components/maps/MapView.jsx
+++ src/components/maps/MapView.jsx
@@ -8,13 +8,15 @@ const MapView = ({ geojsonData }) => {
   return (
     <div className="mapContainer">
       <MapContainer center={[0.5605645791559364, 123.05812205598738]} zoom={13} style={{ height: "100vh" }}>
-        <GeoJSON data={geojsonData.features} />
         <LayersControl position="topright">
           {dataLayers.map((layer) => (
             <LayersControl.BaseLayer key={layer.name} checked={layer.name === "OpenStreetMap"} name={layer.name}>
               <TileLayer url={layer.url} attribution={layer.attribution} />
             </LayersControl.BaseLayer>
           ))}
+          <LayersControl.Overlay name="Jaringan Irigasi Lomaya Alale Pilohayanga">
+            <GeoJSON data={geojsonData.features} />
+          </LayersControl.Overlay>
         </LayersControl>
       </MapContainer>
     </div>
