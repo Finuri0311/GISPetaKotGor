e83b513b2796dba2ed7b433c1ac43acbf3564a2c
agungdunggio <agngdnggio@gmail.com>
2024-05-12 10:50:42 +0700

    fix: map features fix

--- src/components/maps/MapView.jsx
+++ src/components/maps/MapView.jsx
@@ -1,30 +1,18 @@
 import React from "react";
 
-import { MapContainer, TileLayer, GeoJSON} from "react-leaflet";
+import { MapContainer, TileLayer, GeoJSON } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import L, { map } from "leaflet";
 
-const MapView = ({geojsonData}) =>{
-    return (
-        <div className="mapContainer">
-            <MapContainer 
-                center={[0.5605645791559364, 123.05812205598738]}
-                zoom={13}
-                style={{height: "100vh"}}
-            >
-                <TileLayer 
-                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                    attribution="&copy; <a href='https://www.openstreetmap.org/copyrigth'>OpenStreetMap</a> Contributors"
-
-                />
-                <GeoJSON 
-                    data={geojsonData.features[0]}
-                />
-            </MapContainer>
-
-        </div>
-    )
-}
+const MapView = ({ geojsonData }) => {
+  return (
+    <div className="mapContainer">
+      <MapContainer center={[0.5605645791559364, 123.05812205598738]} zoom={13} style={{ height: "100vh" }}>
+        <TileLayer url="https://api.maptiler.com/maps/satellite/256/{z}/{x}/{y}.jpg?key=UwNIQPKbRyGGA9A86zvC" attribution="&copy; <a href='https://www.openstreetmap.org/copyrigth'>OpenStreetMap</a> Contributors" />
+        <GeoJSON data={geojsonData.features} />
+      </MapContainer>
+    </div>
+  );
+};
 
 export default MapView;
-
